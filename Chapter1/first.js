9d1ce05980f5a37ee509a5ff4ab8d029a15eba9e
Preetish Niket <niketpreetish@gmail.com>
2025-05-16 00:04:35 +0530

    ES6 variables

---
+++ Chapter1/first.js
@@ -0,0 +1,47 @@
+console.log("Sejal");
+
+//variables
+
+fullname = "Preetish";
+
+age = 25;
+
+price = 99.99;
+
+a = null;
+
+y = undefined;
+
+isBoolean = true;
+
+console.log(isBoolean);
+
+console.log(935000 + 59000);
+
+//ES6 let,  var, const
+
+var age = 25;
+
+var age = 26;
+
+var age = 27;
+
+console.log(age);
+
+//let
+
+let nameVariable = "preetish";
+
+// let name = niket; // ERROR: cannot declare block scope variable;
+
+console.log(nameVariable);
+
+//const
+
+const PI = 3.14;
+
+// const PI = 413;
+
+// PI = 314;  // ERROR: Assignment to constant variable (uncaught TypeError)
+
+console.log(PI);
